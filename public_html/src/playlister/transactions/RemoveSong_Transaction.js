42dc363c4c874e42a161bf273454c9f10bcc9e17
Derek <derek.yin@stonybrook.edu>
2022-09-04 21:52:45 -0400

    Added More Transaction classes

--- public_html/src/playlister/transactions/RemoveSong_Transaction.js
+++ public_html/src/playlister/transactions/RemoveSong_Transaction.js
@@ -1,14 +1,14 @@
 import jsTPS_Transaction from "../../common/jsTPS.js"
 /**
- * AddSong_Transaction
+ * RemoveSong_Transaction
  * 
- * This class represents a transaction that works with adding a song. 
+ * This class represents a transaction that works with removing a song. 
  * It will be managed by the transaction stack.
  * 
  * @author McKilla Gorilla
  * @author Derek Yin
  */
-export default class AddSong_Transaction extends jsTPS_Transaction {
+export default class RemoveSong_Transaction extends jsTPS_Transaction {
     constructor(initModel, initIndex) {
         super();
         this.model = initModel;
@@ -18,9 +18,11 @@ export default class AddSong_Transaction extends jsTPS_Transaction {
     doTransaction() {
         this.model.addSong();
     }
+
+    
     
     undoTransaction() {
-        this.model.deleteSong(this.index);
+        this.model.deleteSong();
         // this.model.removeSong(index);
     }
 }
